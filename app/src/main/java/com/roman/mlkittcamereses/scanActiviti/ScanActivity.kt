b64214b32d0b85d77a02eed26d5f506664c8b868
pavyk-shnyk <postovoiroma@gmail.com>
2023-05-07 17:58:11 +0600

    wegon

---
+++ app/src/main/java/com/roman/mlkittcamereses/scanActiviti/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.roman.mlkittcamereses.scanActiviti
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.roman.mlkittcamereses.R
+
+class ScanActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
--- app/src/main/java/com/roman/mlkittcamereses/sebture/StartCamera.kt
+++ app/src/main/java/com/roman/mlkittcamereses/sebture/StartCamera.kt
@@ -1,30 +1,65 @@
 package com.roman.mlkittcamereses.sebture
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.roman.mlkittcamereses.R
+import com.roman.mlkittcamereses.scanActiviti.ScanActivity
 
 class StartCamera: Fragment() {
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        { isGranted ->
+            if (isGranted == true) {
+                startScan()
+            } else {
+
+            }
+
+        }
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.start_scan, container, false)
-        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val ssilkaButton = view.findViewById<Button>(R.id.startButtonScan)
         ssilkaButton.setOnClickListener {
-            val entent = Intent (MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivity(entent)
+            val permission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            )
+            if (permission == PackageManager.PERMISSION_GRANTED)
+            {
+                startScan()
+            }   else {
+                permissionLauncher.launch(Manifest.permission.CAMERA)
+            }
         }
     }
+    private fun startScan()
+    {
+        val entent = Intent(requireContext(), ScanActivity::class.java)
+        startActivity(entent)
     }
+
+}
